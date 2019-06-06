c7a6987d4ee04600164aa96f729be8609747fad8
rori4 <RangelStoilov@students.softuni.bg>
2019-06-07 01:57:09 +0300

    blod text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,22 @@ import { Navbar, Nav } from "react-bootstrap"
 import { Link } from "gatsby"
 export default props => (
   <header>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">Gatsby-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/about">
-              About Us
-            </Nav.Link>
-            <Nav.Link as={Link} to="/contact">
-              Contact Us
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Gatsby-Bootstrap</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">
+            <b class="text-dark">Home</b>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about">
+          <b class="text-dark">About Us</b>
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact">
+          <b class="text-dark">Contact Us</b>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   </header>
 )
