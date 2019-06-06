4f5ef67175f6734caac75e34a8566e2b6d11efbf
rori4 <RangelStoilov@students.softuni.bg>
2019-06-07 02:14:58 +0300

    Card.Text dangerouslySetInnerHTML FIX

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { Card, Button } from "react-bootstrap"
 
-const Post = (props) => (
+const Post = props => (
   <post className="p-3">
     <Card>
       <Card.Img variant="top" alt={props.alt || "default"} src={props.image} />
       <Card.Body>
-        <Card.Title dangerouslySetInnerHTML={{__html: props.title}}/>
-        <Card.Text dangerouslySetInnerHTML={{__html: props.excerpt}}/>
-        <Button variant="warning" href={props.readMore}>Read More...</Button>
+        <Card.Title dangerouslySetInnerHTML={{ __html: props.title }} />
+        <Card.Text>
+          <div dangerouslySetInnerHTML={{ __html: props.excerpt }} />
+        </Card.Text>
+        <Button variant="warning" href={props.readMore}>
+          Read More...
+        </Button>
       </Card.Body>
     </Card>
   </post>
